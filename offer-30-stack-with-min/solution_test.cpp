8116a8ae991fb785dbef8bf909076e78f8241805
Pan Taixi <tp1119@ic.ac.uk>
2023-01-27 11:40:38 +0000

    test for min eleme stack

--- offer-30-stack-with-min/solution_test.cpp
+++ offer-30-stack-with-min/solution_test.cpp
@@ -10,4 +10,16 @@ TEST(TestCases, CanFindMinElement) {
   EXPECT_EQ(m, 1);
 }
 
+TEST(TestCases, CanFindMinElementAfterPushPop) {
+  MinStack s;
+  s.push(1);
+  s.push(2);
+  s.push(0);
+  auto m = s.min();
+  EXPECT_EQ(m, 0);
+  s.pop();
+  m = s.min();
+  EXPECT_EQ(m, 1);
+}
+
 
